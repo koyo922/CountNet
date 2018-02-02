9abbdf30ca65e9073e66b86cf3c0e2f26db2b02b
QIAN Weishuo <qianws@fenbi.com>
2018-02-02 17:18:22 +0800

    MOD: refactor script & support padding to 5s

--- predict_audio.py
+++ predict_audio.py
@@ -1,63 +1,69 @@
+import logging
+import argparse
+
 import numpy as np
 import soundfile as sf
-import argparse
-import os
 import keras
 import sklearn
 import librosa
+from keras.preprocessing.sequence import pad_sequences
 
 eps = np.finfo(np.float).eps
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Load keras model and predict speaker count'
-    )
-    parser.add_argument(
-        'audio',
-        help='audio file (16 kHz) of 5 seconds duration'
-    )
 
-    args = parser.parse_args()
-
+# noinspection PyShadowingNames
+def load_model(models_path):
     # load model
-    model = keras.models.load_model(
-        os.path.join('models', 'RNN_keras2.h5')
-    )
-
+    model = keras.models.load_model(models_path)
     # print model configuration
-    model.summary()
+    model.summary(print_fn=logging.debug)
+    return model
 
+
+# noinspection PyShadowingNames
+def load_scaler(scaler_params_path):
     # load standardisation parameters
-    scaler = sklearn.preprocessing.StandardScaler()
-    with np.load(os.path.join("models", 'scaler.npz')) as data:
+    scaler = sklearn.preprocessing.StandardScaler(scaler_params_path)
+    with np.load(scaler_params_path) as data:
         scaler.mean_ = data['arr_0']
         scaler.scale_ = data['arr_1']
+    return scaler
 
+
+# noinspection PyShadowingNames
+def read_in_wav(wav_path, model, scaler):
     # compute audio
-    audio, rate = sf.read(args.audio, always_2d=True)
-
-    # downmix to mono
+    audio, rate = sf.read(wav_path, always_2d=True)
+    audio = pad_sequences([audio], maxlen=80000, dtype=np.float64, padding='post')[0]
+    # down_mix to mono
     audio = np.mean(audio, axis=1)
-
     # max normalise output
     audio /= np.max(audio, axis=0)
-
     # compute STFT
-    X = np.abs(librosa.stft(audio, n_fft=400, hop_length=160)).T
-
+    x = np.abs(librosa.stft(audio, n_fft=400, hop_length=160)).T
     # apply standardization
-    X = scaler.transform(X)
-
+    x = scaler.transform(x)
     # cut to input shape length (500 frames x 201 STFT bins)
-    X = X[:model.input_shape[1], :]
-
+    x = x[:model.input_shape[1], :]
     # apply normalization
-    Theta = np.linalg.norm(X, axis=1) + eps
-    X /= np.mean(Theta)
+    theta = np.linalg.norm(x, axis=1) + eps
+    x /= np.mean(theta)
 
-    # add sample dimension
-    Xs = X[np.newaxis, ...]
+    return x
 
-    # predict output
-    ys = model.predict(Xs, verbose=0)
-    print("Speaker Count Estimate: ", np.argmax(ys, axis=1)[0])
+
+if __name__ == '__main__':
+    # noinspection PyArgumentList
+    parser = argparse.ArgumentParser(description='Load Keras model and predict speaker count')
+    parser.add_argument('--audio', help='audio file (16 kHz) of 5 seconds duration')
+    # args = parser.parse_args()
+    args = parser.parse_args(['--audio', 'examples/1.wav_trim.wav'])
+
+    scaler = load_scaler('models/scaler.npz')
+    model = load_model('models/RNN_keras2.h5')
+
+    x = read_in_wav(args.audio, model, scaler)
+
+    Xs = x[np.newaxis, ...]  # add sample dimension
+    ys = model.predict(Xs, verbose=0)  # predict output
+    logging.info("Speaker Count Estimate: ", np.argmax(ys, axis=1)[0])
