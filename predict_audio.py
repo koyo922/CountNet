3ced53773cfdb8e484e969ebe63fa51dbd9c77a4
QIAN Weishuo <qianws@fenbi.com>
2018-02-05 10:46:01 +0800

    MOD: support batch predict

--- predict_audio.py
+++ predict_audio.py
@@ -1,7 +1,12 @@
+# IDEA找不到，其实能跑(在项目属性页中配置好sys.path后才能识别，直接add to sources root 时好时坏不一定生效)
+from parallel_util import parallel_process
+
 import logging
 import argparse
+from functools import partial
 
 import numpy as np
+import pandas as pd
 import soundfile as sf
 import keras
 import sklearn
@@ -55,15 +60,24 @@ def read_in_wav(wav_path, model, scaler):
 if __name__ == '__main__':
     # noinspection PyArgumentList
     parser = argparse.ArgumentParser(description='Load Keras model and predict speaker count')
-    parser.add_argument('--audio', help='audio file (16 kHz) of 5 seconds duration')
-    # args = parser.parse_args()
-    args = parser.parse_args(['--audio', 'examples/1.wav_trim.wav'])
+    parser.add_argument('--audio', help='audio file (16 kHz) of 5 seconds duration', default='examples/1.wav_trim.wav')
+    parser.add_argument('--df', default='./res/4200f5d1_df_kid_countnet.pkl')
+    parser.add_argument('-l', '--log_level', default='INFO')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
 
     scaler = load_scaler('models/scaler.npz')
     model = load_model('models/RNN_keras2.h5')
 
-    x = read_in_wav(args.audio, model, scaler)
+    if args.df is None:
+        x = read_in_wav(args.audio, model, scaler)
+        x = x[np.newaxis, ...]  # add sample dimension
+    else:
+        df = pd.read_pickle(args.df).head(5)
+        read_in_wav = partial(read_in_wav, model=model, scaler=scaler)
+        x = parallel_process(df.dbPath.values, read_in_wav)
 
-    Xs = x[np.newaxis, ...]  # add sample dimension
-    ys = model.predict(Xs, verbose=0)  # predict output
-    logging.info("Speaker Count Estimate: ", np.argmax(ys, axis=1)[0])
+    y = model.predict(x, verbose=0)  # predict output
+    logging.info("Speaker Count Estimate: %s", np.argmax(y, axis=1))
